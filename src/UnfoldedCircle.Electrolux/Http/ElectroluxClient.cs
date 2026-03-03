2c08ef66c8692e93cf2aef98fd028ec894aa7a1f
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-03-03 21:10:58 +0100

    Fix

--- src/UnfoldedCircle.Electrolux/Http/ElectroluxClient.cs
+++ src/UnfoldedCircle.Electrolux/Http/ElectroluxClient.cs
@@ -245,7 +245,7 @@ public sealed record ApplianceReportedProperty(
     [property: JsonPropertyName("Temp")] short Temperature,
     sbyte Humidity,
     [property: JsonPropertyName("PM1")] ushort Pm1,
-    [property: JsonPropertyName("PM25")] ushort Pm25,
+    [property: JsonPropertyName("PM2_5")] ushort Pm25,
     [property: JsonPropertyName("PM10")] ushort Pm10,
     [property: JsonPropertyName("ECO2")] ushort Eco2
 );
